5f24bac2a74b8a292a667a8e7e7281447b76ccbe
fils07 <mefire023@gmail.com>
2022-09-25 17:08:57 +0200

    change contract address and METADATA_URL

--- back-end/constants/index.js
+++ back-end/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRRESS="0x09e0DBB720811a3B2Cebf39d228aB94e9c033aFF"
-const METADATA_URL="https://nft-of-bamoun-s-kings-git-main-fils07.vercel.app/api/"
+const METADATA_URL="https://nft-of-bamoun-s-kings.vercel.app/api/"
 
 module.exports={WHITELIST_CONTRACT_ADDRRESS,METADATA_URL}
